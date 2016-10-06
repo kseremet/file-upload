6597f86511b8daecf42b94022921bd34d42417a0
kseremet <korayseremet@gmail.com>
2016-10-06 17:13:01 +0300

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -47,4 +47,4 @@ def uploaded_file(filename):
                                filename)
     
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=5001, debug=True)
+    application.run()
